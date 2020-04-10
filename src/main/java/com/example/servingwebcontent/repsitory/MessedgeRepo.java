5fa9f9689630f5b4c735540dd98d674ad84179d4
DenysVolokha <kilbkalol@gmail.com>
2020-04-10 16:32:41 +0300

    fix gitignore

---
+++ src/main/java/com/example/servingwebcontent/repsitory/MessedgeRepo.java
@@ -0,0 +1,6 @@
+package com.example.servingwebcontent.repsitory;
+import com.example.servingwebcontent.domain.Messege;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessedgeRepo extends CrudRepository<Messege, Integer> {
+}
